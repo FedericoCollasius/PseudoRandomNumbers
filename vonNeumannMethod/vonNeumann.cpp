f2e983ff105a5030936b541102ad9a02beeb2d92
Fede <fede.collasius@gmail.com>
2023-07-22 18:46:36 -0300

    VonNeumann listo.

--- vonNeumannMethod/vonNeumann.cpp
+++ vonNeumannMethod/vonNeumann.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-
+//O(log(x))
 int countDigits(long long int x){
     int digits = 0;
     while(x > 0){
@@ -11,6 +11,7 @@ int countDigits(long long int x){
     return digits;
 }
 
+//O(log(seed))
 long long int middleNDigits(long long int seed, int n){
     long long int seed_squared = seed * seed; 
     int totalDigits = countDigits(seed_squared); 
@@ -18,11 +19,14 @@ long long int middleNDigits(long long int seed, int n){
     if(n >= totalDigits){
         return seed_squared;
     }
-
+    // Si el número tiene un total de "totalDigits" dígitos, necesitamos extraer "n" dígitos centrales. Entonces, si calculamos 
+    // "start" como "(totalDigits - n) / 2", nos aseguramos de que los dígitos extraídos sean realmente los dígitos centrales.
     int start = (totalDigits - n) / 2;
-    int end = start + n;
 
+    // Elimina los dígitos menos significativos que están despues de la posición "end" = start + n, dejándonos con los dígitos desde el inicio 
+    // hasta "end".
     seed_squared /= pow(10, start);
+    // Esto eliminará los dígitos mas significativos que están desde el inicio hasta "start", dejándonos con los "n" dígitos centrales.
     seed_squared %= (int) pow(10, n);
 
     if(seed_squared == 0 || seed_squared == 1){
